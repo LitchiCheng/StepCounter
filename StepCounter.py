cb1913e6d415bdc4beb5dcbe2677c90c072c03c8
darboy <974782852@qq.com>
2020-05-21 22:50:23 +0800

    完成计步算法，并测试成功

--- StepCounter.py
+++ StepCounter.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.signal as signal
-import serial,re,time,math
+import serial,re,time,math,socket
 
 class recDataBase:
     '''
@@ -42,15 +42,40 @@ class recDataBySerial(recDataBase):
         by serial
     '''
     def __init__(self, com, baudrate, slide_size, pattern, data_index):
-        super.__init__(slide_size)
+        super().__init__(slide_size)
         self.ser=serial.Serial(com, baudrate)
         self.__pattern = pattern
         self.__data_index = data_index
 
     def rec(self):
         data_str = self.ser.readline()
+        data_str = data_str.decode("utf-8")
         data = re.split(self.__pattern, data_str)[self.__data_index]
-        super.insert(data)
+        super().insert(data)
+        return float(data)
+
+class recDataByTcp(recDataBase):
+    '''
+        by tcp
+    '''
+    def __init__(self,ip,port,slide_size,pattern,data_index):
+        super().__init__(slide_size)
+        self.ip = ip
+        self.port = port
+        self.__pattern = pattern
+        self.__data_index = data_index
+        server_addr = (ip,port)
+        server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        server.bind(server_addr)
+        server.listen(5)
+        self.ss, addr = server.accept() 
+    
+    def rec(self):
+        data_str = self.ss.recv()
+        data_str = data_str.decode("utf-8")
+        data = re.split(self.__pattern, data_str)[self.__data_index]
+        super().insert(data)
+        return float(data)
 
 class stepCounter:
     def __init__(self, angle_threshold, valid_time):
@@ -60,20 +85,18 @@ class stepCounter:
         '''
         self.__angle_td = angle_threshold
         self.__valid_t = valid_time
-        # self.rec_data = recDataBySerial("COM2",115200,20,"[|\n]",1)
-        self.rec_data = recDataBase(20)
+        self.rec_data = recDataBySerial("COM7",115200,20,"[\r\n]",0)
+        # self.rec_data = recDataBase(20)
         self.last_step_t = time.time()
         self.start_step_t = time.time()
         self.valid_step_record = []
         self.step_count = 0
         self.count = 0
+        self.is_new_step = False
 
     def run(self):
-        self.rec_data.rec()
-        self.count = self.count + 1
-        self.rec_data.insert(math.sin(self.count))
-        filter_data = self.rec_data.get()
-        print(filter_data)
+        filter_data = self.rec_data.rec()
+        # filter_data = self.rec_data.get()
         if filter_data < self.__angle_td:
             self.start_step_t = time.time()
             self.valid_step_record.clear()
@@ -85,8 +108,7 @@ class stepCounter:
                 self.last_step_t = time.time()
                 self.is_new_step = False
                 # signal.argrelextrema(self.valid_step_record, np.greater_equal)
-        print("now step count is " + str(self.step_count))
-
+        print("pitch is %03f, step is %d" % (filter_data, self.step_count))
 
 if __name__ == "__main__":
     # size = 200
@@ -95,7 +117,7 @@ if __name__ == "__main__":
     #     import math
     #     ex.insert(math.cos(i))
     # print(ex.get())
-    ex = stepCounter(0.05,0.1)
+    ex = stepCounter(30.0,0.5)
     while True:
         ex.run()
-        time.sleep(0.1)
+        # time.sleep(0.1)
