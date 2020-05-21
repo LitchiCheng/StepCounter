715969d8139ba1bb60868cc0eb071e303f8546a9
darboy <974782852@qq.com>
2020-05-21 23:34:09 +0800

    初步完成

--- StepCounter.py
+++ StepCounter.py
@@ -1,123 +1,90 @@
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+from pyqtgraph.Point import Point
+import re, time, os, sys
 import numpy as np
-import scipy.signal as signal
-import serial,re,time,math,socket
 
-class recDataBase:
-    '''
-        recData base
-        override: func __init__() to add pattern to fetch useful data
-        override: func rec() to realize other ways of transmission
-    '''
-    def __init__(self, slide_size):
-        self.__slide_size = slide_size
-        self.__data = np.zeros(slide_size, float)
+class ScrollingPloter:
+    def __init__(self, title_name, plot_num, x_data_num = 300, refresh_rate = 1):
+        self.x_data_num = x_data_num
+        self.title_name = title_name
+        self.plot_num = plot_num
+        self.refresh_rate = refresh_rate
+        self.win = pg.GraphicsWindow()
+        self.win.setWindowTitle(self.title_name)
+        self.plot_array = []
+        self.label = pg.LabelItem()
+        self.win.addItem(self.label)
+        self.start_step_t = time.time()
+        self.last_step_t = time.time()
+        self.is_new_step = False
+        self.step_count = 0
+        self.curve_array = []
+        self.data_array = []
+        self.timer_array = []
+        self.slot_func_array = []
+        self.func = []
+        for i in range(0, plot_num):
+            p = self.win.addPlot(row=i,col=0)
+            p.showGrid(x=True,y=True)
+            self.plot_array.append(p)
+            init_data = np.zeros(x_data_num)
+            self.data_array.append(init_data)
+            curve = p.plot(self.data_array[i], pen="y", symbolBrush=(255,0,0), symbolPen='w')
+            self.win.nextRow()
+            self.curve_array.append(curve)
+            self.func.append(self.noFunc)
 
-    def insert(self, data):
-        self.__data[:-1] = self.__data[1:]
-        try:
-            data = float(data)
-        except:
-            data = self.__data[-2]
-            self.log("data convert to float failed:"+str(data))
-        self.__data[-1] = data
-    
-    def rec(self):
+    def noFunc(self):
         pass
 
-    def get(self):
-        return np.mean(self.__data)
-        # return self.__data
+    def setFunc(self, plot_index, func):
+        self.timer = pg.QtCore.QTimer()
+        self.func[plot_index] = func
+        self.timer.timeout.connect(func)
+        self.timer.start(1)
 
-    def size(self):
-        return self.__slide_size
+'''串口接收
+********************************************************
+'''
+import serial
+ser=serial.Serial("COM7",115200)
 
-    def clear(self):
-        self.__data = np.zeros(self.__slide_size)
 
-    def log(self,str):
-        print(str)
-
-class recDataBySerial(recDataBase):
-    '''
-        by serial
-    '''
-    def __init__(self, com, baudrate, slide_size, pattern, data_index):
-        super().__init__(slide_size)
-        self.ser=serial.Serial(com, baudrate)
-        self.__pattern = pattern
-        self.__data_index = data_index
-
-    def rec(self):
-        data_str = self.ser.readline()
-        data_str = data_str.decode("utf-8")
-        data = re.split(self.__pattern, data_str)[self.__data_index]
-        super().insert(data)
-        return float(data)
-
-class recDataByTcp(recDataBase):
-    '''
-        by tcp
-    '''
-    def __init__(self,ip,port,slide_size,pattern,data_index):
-        super().__init__(slide_size)
-        self.ip = ip
-        self.port = port
-        self.__pattern = pattern
-        self.__data_index = data_index
-        server_addr = (ip,port)
-        server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        server.bind(server_addr)
-        server.listen(5)
-        self.ss, addr = server.accept() 
+def update():
+    data_str = ser.readline()
+    data_str = data_str.decode("utf-8")
+    data = re.split("[\r\n]", data_str)[0]
+    data = float(data)
+    x.data_array[0][:-1] = x.data_array[0][1:]
+    x.data_array[0][-1] = data
+    x.curve_array[0].setData(x.data_array[0])
+    angle_td = 30.0
+    valid_t = 0.5
     
-    def rec(self):
-        data_str = self.ss.recv()
-        data_str = data_str.decode("utf-8")
-        data = re.split(self.__pattern, data_str)[self.__data_index]
-        super().insert(data)
-        return float(data)
+    filter_data = data
+    if filter_data < angle_td:
+        x.start_step_t = time.time()
+        x.is_new_step = True
+    else:
+        if (time.time() - x.start_step_t >= valid_t) and x.is_new_step:
+            x.step_count = x.step_count + 1
+            x.last_step_t = time.time()
+            x.is_new_step = False
 
-class stepCounter:
-    def __init__(self, angle_threshold, valid_time):
-        '''
-            angle_threshold: minimun angle at which the legs stand upright and step (degree)
-            valid_time: time between two steps (ms)
-        '''
-        self.__angle_td = angle_threshold
-        self.__valid_t = valid_time
-        self.rec_data = recDataBySerial("COM7",115200,20,"[\r\n]",0)
-        # self.rec_data = recDataBase(20)
-        self.last_step_t = time.time()
-        self.start_step_t = time.time()
-        self.valid_step_record = []
-        self.step_count = 0
-        self.count = 0
-        self.is_new_step = False
+    x.label.setText(
+        "<span style='font-size: 50pt; color: white'>step count is %d<span style='color: white'></span>" % (
+            x.step_count))
+'''
+*********************************************************
+'''
 
-    def run(self):
-        filter_data = self.rec_data.rec()
-        # filter_data = self.rec_data.get()
-        if filter_data < self.__angle_td:
-            self.start_step_t = time.time()
-            self.valid_step_record.clear()
-            self.is_new_step = True
-        else:
-            self.valid_step_record.append(filter_data)
-            if (time.time() - self.start_step_t >= self.__valid_t) and self.is_new_step:
-                self.step_count = self.step_count + 1
-                self.last_step_t = time.time()
-                self.is_new_step = False
-                # signal.argrelextrema(self.valid_step_record, np.greater_equal)
-        print("pitch is %03f, step is %d" % (filter_data, self.step_count))
+x = ScrollingPloter("setp counter", 1)
+timer = pg.QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(1)
 
-if __name__ == "__main__":
-    # size = 200
-    # ex = recDataBase(size)
-    # for i in range(1,size):
-    #     import math
-    #     ex.insert(math.cos(i))
-    # print(ex.get())
-    ex = stepCounter(30.0,0.5)
-    while True:
-        ex.run()
-        # time.sleep(0.1)
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
